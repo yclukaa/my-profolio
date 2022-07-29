f98189669f16dd7d1addb8769470237d8135dfcd
Issac Luk <yinchai6@gmail.com>
2022-07-29 12:12:20 +0800

    Completed AboutMe page with ImageGallery left.
    Completed AboutMe page without responsive design with ImageGallery left.

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,8 +1,103 @@
+import { useState } from "react";
 
+import CallToAction from "../components/cta/CallToAction";
+import Switch from "../components/Switch/Switch";
+import FigmaIcon from "../components/icons/FigmaIcon";
+import AdobeIllustratorIcon from "../components/icons/AdobeIllustratorIcon";
+import VSCodeIcon from "../components/icons/VSCodeIcon";
+import PythonIcon from "../components/icons/PythonIcon";
+import GitIcon from "../components/icons/GitIcon";
+import Card from "../components/ui/Card";
+import classes from "./AboutMe.module.css";
 
 function AboutMePage() {
-    return(<div>About Me Page</div>);
+  const [activeTabID, setActiveTabID] = useState(0);
+  function tabOnClickHandler(id) {
+    setActiveTabID(id);
+  }
 
+  const tabs = [
+    {
+      id: 0,
+      text: "Tools I Use for Design",
+      toolsData: [
+        { icon: <FigmaIcon />, name: "Figma" },
+        { icon: <AdobeIllustratorIcon />, name: "Adobe Illustrator" },
+      ],
+    },
+    {
+      id: 1,
+      text: "Tools I Use for Development",
+      toolsData: [
+        { icon: <VSCodeIcon />, name: "VS Code" },
+        { icon: <PythonIcon />, name: "Python" },
+        { icon: <GitIcon />, name: "Git" },
+      ],
+    },
+  ];
+
+  const tabActiveStyle = {
+    color: "#ffffff",
+    backgroundColor: "#262F36",
+    boxShadow: "0px 4px 4px 6px rgba(38, 47, 54, 0.25)",
+  };
+  const tabInactiveStyle = {
+    color: "rgba(38, 47, 54, 0.75)",
+    cursor: "pointer",
+  };
+
+  return (
+    <div className={classes.aboutMe}>
+      <div className={classes.hero}>
+        <div className={classes.heroText}>
+          <h2>I’m Issac Luk.</h2>
+          <div className={classes.heroParagraph}>
+            <p>
+              I’m interested in fintech. I built a system for predicting stock
+              price and offering financial advice with my teammates in my final
+              year project.
+            </p>
+            <p>I am able to design and code responsive website.</p>
+            <p>
+              I had the pleasure of working in the Hong Kong Jockey Club as an
+              intern.
+            </p>
+            <p>I’m always eager to learn and ready for challenges.</p>
+          </div>
+        </div>
+        <div className={classes.imageGallery}></div>
+      </div>
+      <div className={classes.myTools}>
+        <Switch
+          tabs={tabs}
+          activeTabID={activeTabID}
+          tabOnClick={tabOnClickHandler}
+          tabActiveStyle={tabActiveStyle}
+          tabInactiveStyle={tabInactiveStyle}
+        />
+        <div className={classes.myToolsList}>
+          {tabs[activeTabID].toolsData.map((toolData) => (
+            <Card
+              key={toolData.name}
+              cardStyle={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: "32px",
+                gap: "16px",
+                backgroundColor: "rgba(38, 47, 54, 0.25)",
+              }}
+            >
+              {toolData.icon}
+              <p style={{fontSize: "18px", lineHeight: "21px"}}>{toolData.name}</p>
+            </Card>
+          ))}
+        </div>
+      </div>
+      <CallToAction />
+    </div>
+  );
 }
 
 export default AboutMePage;
